7441b75b519380668907c91a6cb5a109709cf979
Fabrice DUJARDIN <fdujardin@giep.nc>
2021-08-25 14:57:24 +1100

    update

--- classes/Staff.php
+++ classes/Staff.php
@@ -19,15 +19,5 @@ class Staff extends Database
         var_dump($staffEmail);
         return $staffEmail->fetch();
     }
-    // public static function read($id)
-    // {
-    //     $filmBylanguage = self::query("SELECT * FROM actor WHERE actor_id = $id");
-    //     return $filmBylanguage->fetch();
-    // }
-    // public static function readByFilm($id) {
-    //     $test = self::query("SELECT * FROM actor, film_actor WHERE actor.actor_id = film_actor.actor_id AND film_id = $id");
-    //     // var_dump($test);
-    //     return $test->fetchall();
-    // }
     
 }
