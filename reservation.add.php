40e7a6db84ba864335b7897853dea19da3435431
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-08-30 07:39:18 +1100

    update

--- reservation.add.php
+++ reservation.add.php
@@ -14,6 +14,7 @@ require './classes/Staff.php';
 require './classes/Store.php';
 require './classes/Customer.php';
 require './classes/Inventory.php';
+require './classes/Address.php';
 echo template_header('Read all rental', 'rubrique2');
 ?>
 <?php
@@ -22,24 +23,40 @@ if (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] == false) :
 <?php
     header("refresh:0; /login.php");
 else :
+
+    $stores = Store::all();
+    var_dump($stores);
+    // // // // Shamps in Store 
+    // store_id
+    // manager_staff_id
+    // address_id
 ?>
     <section>
         <div class="container pt-3 mt-5">
             <form action="" method="post">
                 <h2>Formulaire pour un nouvel enregistrement</h2>
-                <label for="magasin"></label>
+                <label for="magasin">Address du magasin de la location</label>
                 <select name="stor" id="stor_id">
                     <?php   
-                        $stores = Store::all();
-                        var_dump($stores);
                         foreach ($stores as $store) {
-                            var_dump($store);
-                            echo'<option value=""></option>';
+                            $StoreId = (int)$store['store_id'];  
+                            $adressNames = Address::read($StoreId);
+                            $adressName = $adressNames['address']; 
+                            echo "<option value='$StoreId'>$adressName</option>";
                         }
                     ?>
-                    <option value=""></option>
                 </select>
-                <label for="staff"></label>
+                <label for="staff">Manager qui effectue la location</label>
+                <select>
+                    <?php
+                        foreach ($stores as $store) {
+                            $StoreManager_staff_id = (int)$store['manager_staff_id'];
+                            $staffId = Staff::read($StoreManager_staff_id);
+                            $staffName = $staffId['first_name'];
+                            echo "<option value='$StoreManager_staff_id'>$staffName</option>";
+                        }
+                    ?>
+                </select>
             </form>
         </div>
     </section>
